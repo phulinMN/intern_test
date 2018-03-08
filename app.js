5ed9eff74008f188fe764430790b498b5cebd4fe
phulinMN <pongpaiMN@gmail.com>
2018-03-08 15:29:02 +0700

    sum page

--- app.js
+++ app.js
@@ -32,54 +32,30 @@ connection.connect(function(err){
 });
 
 app.get('/',function(req,res){
-    res.render('index');
-});
-
-app.get("/test1",function(req,res){
-    let sql = 'SELECT user.name AS user,COUNT(visit.user_id) AS count_user, AVG(HOUR(TIMEDIFF(visit.time_in,visit.time_out))+MINUTE(TIMEDIFF(visit.time_in,visit.time_out))/60) AS time_avg, DATEDIFF(MAX(visit.time_in),MIN(visit.time_in)) AS time_range FROM visit JOIN user ON visit.user_id = user.id GROUP BY user_id';
-    connection.query(sql, function(err, rows, fields) {
-        if (!err){
-            console.log('The solution is: ', rows);
-            res.render('page1',{
-                title: 'TEST1',
-                table: rows
-            });
-        }
-        else
-            console.log('Error while performing Query.');
+    let sql1 = 'SELECT user.name AS user,COUNT(visit.user_id) AS count_user, AVG(HOUR(TIMEDIFF(visit.time_in,visit.time_out))+MINUTE(TIMEDIFF(visit.time_in,visit.time_out))/60) AS time_avg, DATEDIFF(MAX(visit.time_in),MIN(visit.time_in)) AS time_range FROM visit JOIN user ON visit.user_id = user.id GROUP BY user_id';
+    let sql2 = 'SELECT service.name, COUNT(IFNULL(service_id,0)) AS count_service FROM visit LEFT JOIN service ON visit.service_id = service.id GROUP BY service_id';
+    let sql3 = 'SELECT user.name AS user, visit.service_id, service.name AS service, IFNULL(visit.service_id,0) AS service_id, COUNT(IFNULL(visit.service_id,0)) AS count_service, DATE_FORMAT(visit.time_in, "%d %m %T") AS time_in, DATE_FORMAT(visit.time_out, "%d %m %T") AS time_out, AVG(HOUR(TIMEDIFF(visit.time_out,visit.time_in))*60 + MINUTE(TIMEDIFF(visit.time_out,visit.time_in))) AS time_avg FROM visit LEFT JOIN user ON visit.user_id = user.id LEFT JOIN service ON visit.service_id = service.id WHERE HOUR(TIMEDIFF(visit.time_out,visit.time_in))*60 + MINUTE(TIMEDIFF(visit.time_out,visit.time_in)) > 300 GROUP BY user_id,service_id';
+    connection.query(sql1, function(err1, rows1, fields) {
+        connection.query(sql2, function(err2, rows2, fields) {
+            if (!err1){
+                if (!err2){
+                    connection.query(sql3, function(err3, rows3, fields) {
+                        if (!err3){
+                            res.render('index',{
+                                title: 'TEST',
+                                table1: rows1,
+                                table2: rows2,
+                                table3: rows3
+                            });
+                        }
+                    }); 
+                }
+            }
+        });
     });
+    
+    
 });
-
-app.get("/test2",function(req,res){
-    let sql = 'SELECT service.name, COUNT(IFNULL(service_id,0)) AS count_service FROM visit LEFT JOIN service ON visit.service_id = service.id GROUP BY service_id';
-    connection.query(sql, function(err, rows, fields) {
-        if (!err){
-            console.log('The solution is: ', rows);
-            res.render('page2',{
-                title: 'TEST2',
-                table: rows
-            });
-        }
-        else
-            console.log('Error while performing Query.');
-    });
-});
-
-app.get("/test3",function(req,res){
-    let sql = 'SELECT user.name AS user, visit.service_id, service.name AS service, IFNULL(visit.service_id,0) AS service_id, COUNT(IFNULL(visit.service_id,0)) AS count_service, DATE_FORMAT(visit.time_in, "%d %m %T") AS time_in, DATE_FORMAT(visit.time_out, "%d %m %T") AS time_out, AVG(HOUR(TIMEDIFF(visit.time_out,visit.time_in))*60 + MINUTE(TIMEDIFF(visit.time_out,visit.time_in))) AS time_avg FROM visit LEFT JOIN user ON visit.user_id = user.id LEFT JOIN service ON visit.service_id = service.id WHERE HOUR(TIMEDIFF(visit.time_out,visit.time_in))*60 + MINUTE(TIMEDIFF(visit.time_out,visit.time_in)) > 300 GROUP BY user_id,service_id';
-    connection.query(sql, function(err, rows, fields) {
-        if (!err){
-            console.log('The solution is: ', rows);
-            res.render('page3',{
-                title: 'TEST3',
-                table: rows
-            });
-        }
-        else
-            console.log('Error while performing Query.');
-    });
-});
-
 
 
 app.listen(3000, function(){
